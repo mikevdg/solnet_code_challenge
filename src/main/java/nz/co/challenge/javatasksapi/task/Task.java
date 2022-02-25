4ccd235aaaadb1ac90dd64a3a8054679b1d7c314
Michael van der Gulik <mikevdg@gulik.co.nz>
2022-02-26 03:20:21 +1300

    Added README notes. Fixed up some REST behaviour.

--- src/main/java/nz/co/challenge/javatasksapi/task/Task.java
+++ src/main/java/nz/co/challenge/javatasksapi/task/Task.java
@@ -1,5 +1,8 @@
 package nz.co.challenge.javatasksapi.task;
 
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,20 +13,22 @@ import java.util.Date;
 public class Task {
     @Id
     @GeneratedValue
-    private Long id;
+    public Long id;
 
-    @Column(length=256, nullable=false)
-    private String title; // 256
+    @Column(length=256) // , nullable=false
+    public String title; // 256
 
     @Column(length=1024)
-    private String description; // 1024
+    public String description; // 1024
 
     @Column(name="due_date", columnDefinition = "DATE")
-    private Date dueDate;
+    @DateTimeFormat(iso=DateTimeFormat.ISO.DATE)
+    public Date dueDate;
 
     @Column(length=10)
-    private String status; // 10;
+    public String status; // 10;
 
-    @Column(name="creation_date", columnDefinition="DATE", nullable=false)
-    private Date creationDate;
+    @Column(name="creation_date", columnDefinition="DATE") // , nullable=false
+    @DateTimeFormat(iso=DateTimeFormat.ISO.DATE)
+    public Date creationDate;
 }
--- src/main/java/nz/co/challenge/javatasksapi/task/TaskController.java
+++ src/main/java/nz/co/challenge/javatasksapi/task/TaskController.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Date;
+
 @RestController
 @RequestMapping("/task")
 public class TaskController {
@@ -29,13 +31,16 @@ public class TaskController {
     }
 
     @PostMapping("/")
-    public ResponseEntity<Task> createTask(Task t) {
+    public ResponseEntity<Task> createTask(@RequestBody Task t) {
+        t.creationDate = new Date(); // Today.
         taskRepository.save(t);
         return new ResponseEntity<>(t, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Task> modifyTask(Task t, @PathVariable Long id) {
+    public ResponseEntity<Task> modifyTask(@RequestBody Task t, @PathVariable Long id) {
+        Task orig = taskRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        t.creationDate = orig.creationDate;
         taskRepository.save(t);
         return new ResponseEntity<>(t, HttpStatus.CREATED);
     }
@@ -45,6 +50,7 @@ public class TaskController {
         if (null==id) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "id must be provided.");
         }
-        taskRepository.deleteById(id); // TODO: What if it doesn't exist?
+        taskRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        taskRepository.deleteById(id);
     }
 }
