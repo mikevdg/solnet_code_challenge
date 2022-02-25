0b8ddccf3ab9ff5879f48e2d2360525a24db04d1
Michael van der Gulik <mikevdg@gulik.co.nz>
2022-02-25 23:40:51 +1300

    Spring boot project skeleton.

---
+++ src/main/java/nz/co/challenge/javatasksapi/JavaTaskApiChallengeApplication.java
@@ -0,0 +1,13 @@
+package nz.co.challenge.javatasksapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTaskApiChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTaskApiChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nz/co/challenge/javatasksapi/JavaTaskApiChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package nz.co.challenge.javatasksapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTaskApiChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
